85b8017c099c7479c1cac3db5a0324f4624ab793
Bruno de Oliveira <brunodoliveirapereira@gmail.com>
2020-03-17 23:36:52 -0300

    criado arquivos index e rotas

---
+++ routes/pedidos.js
@@ -0,0 +1,49 @@
+//IMPORTADOS
+const express = require('express');
+const router = express.Router();
+
+
+///GET DE TODOS
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: "Pedidos Usando o Get "
+    })
+});
+///POST INSERIR
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: " Pedidos  Usando o Post "
+    })
+});
+//////GET POR ID
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto
+
+    if (id === 'especial') {
+        res.status(200).send({
+            mensagem: " Pedidos Usando o Get Por ID ESPECIAL  ",
+            id: id
+        });
+    } else {
+        res.status(200).send({
+            mensagem: "Pedidos Voce GET POR ID  "
+
+        })
+    }
+});
+
+// PATCH ALTERAR 
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: " Pedidos Usando o Patch "
+    })
+});
+
+// DELETE  EXCLUIR
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: "Pedidos Usando o Delete "
+    })
+});
+
+module.exports = router;
